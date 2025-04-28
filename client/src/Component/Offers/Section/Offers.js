113a8e307be5a78c1800aed1f7e6b09712333596
dalia-hegazy <daliahegazy346@gmail.com>
2025-04-29 00:17:30 +0300

    Offers

---
+++ client/src/Component/Offers/Section/Offers.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+const Offers = () => {
+  
+    return (
+      <div>
+      <h1>
+        lorem
+      </h1>
+      </div>
+    );
+  
+};
+
+export default Offers;
