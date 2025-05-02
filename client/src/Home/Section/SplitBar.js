0655c83cd20d6fba067b401f76347c600b1f6040
RaDwan <119738657+rm1167616@users.noreply.github.com>
2025-05-02 13:22:17 +0300

    radwan

--- client/src/Home/Section/SplitBar.js
+++ client/src/Home/Section/SplitBar.js
@@ -5,98 +5,12 @@ import Logo from '../../Assetes/imgs/Logo.png'
 const SplitPage = () => {
   return (
     <Container fluid style={{ height: '11vh', marginBottom: '1%', marginTop: '0%' }}>
-      <Row style={{ height: '100%' }}>
-        {/* Left Side - 25% width (since you have 4 columns) */}
-        <Col md={3} style={{ 
-          height: '100%', 
-          backgroundColor: '#231f20',
-          display: 'flex',
-          alignItems: 'center', // Vertically center items
-          justifyContent: 'center' // Horizontally center items
-        }}>
-          <div style={{
-            display: 'flex',
-            alignItems: 'center',
-            gap: '1rem' // Adds space between the h6 and img
-          }}>
-           
-            <img 
-              src={Logo} 
-              alt="Backlink Group" 
-              width="80"
-              className="logo-img"
-            />
-            <h6 style={{ fontSize: '30px', fontWeight: '', color: '#917243', margin: 0 }}>    </h6>
-          </div>
-        </Col>
-        
-        <Col md={3} style={{ 
-          height: '100%', 
-          backgroundColor: '#231f20',
-          display: 'flex',
-          alignItems: 'center', // Vertically center items
-          justifyContent: 'center' // Horizontally center items
-        }}>
-          <div style={{
-            display: 'flex',
-            alignItems: 'center',
-            gap: '1rem' // Adds space between the h6 and img
-          }}>
-           
-            <img 
-              src={Logo} 
-              alt="Backlink Group" 
-              width="80"
-              className="logo-img"
-            />
-            <h6 style={{ fontSize: '30px', fontWeight: '', color: '#917243', margin: 0 }}>    </h6>
-          </div>
-        </Col>
-        <Col md={3} style={{ 
-          height: '100%', 
-          backgroundColor: '#231f20',
-          display: 'flex',
-          alignItems: 'center', // Vertically center items
-          justifyContent: 'center' // Horizontally center items
-        }}>
-          <div style={{
-            display: 'flex',
-            alignItems: 'center',
-            gap: '1rem' // Adds space between the h6 and img
-          }}>
-           
-            <img 
-              src={Logo} 
-              alt="Backlink Group" 
-              width="80"
-              className="logo-img"
-            />
-            <h6 style={{ fontSize: '30px', fontWeight: '', color: '#917243', margin: 0 }}>    </h6>
-          </div>
-        </Col>
-        <Col md={3} style={{ 
-          height: '100%', 
-          backgroundColor: '#231f20',
-          display: 'flex',
-          alignItems: 'center', // Vertically center items
-          justifyContent: 'center' // Horizontally center items
-        }}>
-          <div style={{
-            display: 'flex',
-            alignItems: 'center',
-            gap: '1rem' // Adds space between the h6 and img
-          }}>
-           
-            <img 
-              src={Logo} 
-              alt="Backlink Group" 
-              width="80"
-              className="logo-img"
-            />
-            <h6 style={{ fontSize: '30px', fontWeight: '', color: '#917243', margin: 0 }}>     </h6>
-          </div>
-        </Col>
-      </Row>
+    <hr style={{
+  height: '11px',
+  backgroundColor: '#111', // Very dark black
+  border: 'none',
+  margin: '20px 0'
+}} />
     </Container>
   );
 };
