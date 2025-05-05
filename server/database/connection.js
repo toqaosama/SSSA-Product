35c39ca0cc42babb9a25d6c1359a4ef4811ac4dd
RaDwan <119738657+rm1167616@users.noreply.github.com>
2025-05-05 19:26:31 +0300

    RDWANNN

--- server/database/connection.js
+++ server/database/connection.js
@@ -2,17 +2,23 @@ const mysql = require('mysql');
 
 const connection = mysql.createConnection({
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     port: process.env.DB_PORT,
 =======
+=======
+>>>>>>> Stashed changes
   host: '127.0.0.1', // or 'localhost'
   port: 3306,
   user: 'radwan', // usually 'root' for local development
   password: 'RDRsWBWp1Tdv8eQUW5XS',
   database: 'ssa'
+<<<<<<< Updated upstream
+>>>>>>> Stashed changes
+=======
 >>>>>>> Stashed changes
 });
 
